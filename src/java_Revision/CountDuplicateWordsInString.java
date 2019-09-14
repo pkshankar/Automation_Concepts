a116c0df01d15219cbd9318fe5ff4d30af2c5c6a
P K Shankar <pkshank@publicisgroupe.net>
2019-09-14 14:00:46 +0530

    adding count duplicate words in string

--- src/java_Revision/CountDuplicateWordsInString.java
+++ src/java_Revision/CountDuplicateWordsInString.java
@@ -7,7 +7,8 @@ public class CountDuplicateWordsInString {
 
 	public static void main(String[] args) {
 
-		String str = "TESTING WITH SELENIUM AND SELENIUM WITH TESTING INDIA IS A SPACE SUPERPOWER AND INDIA WILL SOON BECOME A DEVELOPED NATION";
+		//String str = "TESTING WITH SELENIUM AND SELENIUM WITH TESTING INDIA IS A SPACE SUPERPOWER AND INDIA WILL SOON BECOME A DEVELOPED NATION";
+		String str = "Prime Minister Narendra Modi will visit the U.S. between September 21 and September 27 during which he will address the annual U.N. General Assembly session and have a series of bilateral and multilateral engagements in New York, the Ministry of External Affairs said on September 12.";
 		String strArr[] = str.split(" ");
 		Map<String, Integer> map = new HashMap<String, Integer>();
 		for (int i = 0; i < strArr.length; i++) {
