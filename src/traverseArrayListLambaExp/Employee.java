ce1cd9d0f3530e10b94206b7e11cf5a996ea9bae
P K Shankar <pkshank@publicisgroupe.net>
2020-03-01 17:17:48 +0530

    adding comparable and comparator

--- src/traverseArrayListLambaExp/Employee.java
+++ src/traverseArrayListLambaExp/Employee.java
@@ -1,6 +1,6 @@
 package traverseArrayListLambaExp;
 
-public class Employee {
+public class Employee implements Comparable<Employee> {
 
 	private int empId;
 	private String empName;
@@ -23,6 +23,66 @@ public class Employee {
 
 	public String getEmpDesignation() {
 		return empDesignation;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((empDesignation == null) ? 0 : empDesignation.hashCode());
+		result = prime * result + empId;
+		result = prime * result + ((empName == null) ? 0 : empName.hashCode());
+		return result;
+	}
+
+//	public int hashCode() {
+//
+//		return this.getEmpId();
+//	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (empDesignation == null) {
+			if (other.empDesignation != null)
+				return false;
+		} else if (!empDesignation.equals(other.empDesignation))
+			return false;
+		if (empId != other.empId)
+			return false;
+		if (empName == null) {
+			if (other.empName != null)
+				return false;
+		} else if (!empName.equals(other.empName))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int compareTo(Employee emp) {
+
+		if (this.getEmpId() > emp.getEmpId()) {
+
+			return 1;
+		}
+
+		else if (this.getEmpId() < emp.getEmpId()) {
+
+			return -1;
+
+		}
+
+		else {
+
+			return 0;
+		}
+
 	}
 
 }
--- src/traverseArrayListLambaExp/TestEmployee.java
+++ src/traverseArrayListLambaExp/TestEmployee.java
@@ -1,6 +1,8 @@
 package traverseArrayListLambaExp;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
@@ -14,6 +16,7 @@ public class TestEmployee {
 		Employee emp3 = new Employee(201, "Swan", "Senior Director");
 		Employee emp4 = new Employee(119, "Shelgun", "Junior Engineer");
 		Employee emp5 = new Employee(198, "Hilter", "Executive");
+		Employee emp6 = new Employee(198, "Hilter", "Executive");
 
 		List<Employee> listEmployee = new ArrayList<>();
 		listEmployee.add(emp1);
@@ -21,31 +24,87 @@ public class TestEmployee {
 		listEmployee.add(emp3);
 		listEmployee.add(emp4);
 		listEmployee.add(emp5);
+		listEmployee.add(emp6);
 
-		// USING FOR EACH
+		Comparator<Employee> empIdComparator = new Comparator<Employee>() {
 
+			@Override
+			public int compare(Employee o1, Employee o2) {
+
+				if (o1.getEmpId() > o2.getEmpId())
+					return 1;
+				else if (o1.getEmpId() < o2.getEmpId())
+					return -1;
+				else
+					return 0;
+			}
+
+		};
+
+		Comparator<Employee> empNameComparator = new Comparator<Employee>() {
+
+			@Override
+			public int compare(Employee o1, Employee o2) {
+
+				return o1.getEmpName().compareTo(o2.getEmpName());
+
+			}
+
+		};
+
+		Comparator<Employee> empDesignationComparator = new Comparator<Employee>() {
+
+			@Override
+			public int compare(Employee o1, Employee o2) {
+
+				return o1.getEmpDesignation().compareTo(o2.getEmpDesignation());
+
+			}
+
+		};
+
+		//Collections.sort(listEmployee, empIdComparator);
+		//Collections.sort(listEmployee, empNameComparator);
+		Collections.sort(listEmployee, empDesignationComparator);
+		
 		listEmployee.forEach(emp -> {
 
 			System.out.println(emp.getEmpId() + " : " + emp.getEmpName() + " : " + emp.getEmpDesignation());
 		});
 
-		System.out.println("***********************************************************************");
-		// USING LIST ITERATOR
-		ListIterator<Employee> listIteratorEmployee = listEmployee.listIterator(listEmployee.size());
-		while (listIteratorEmployee.hasPrevious()) {
+		// Collections.sort(listEmployee);
 
-			Employee e = listIteratorEmployee.previous();
-			System.out.println(e.getEmpId() + " : " + e.getEmpName() + " : " + e.getEmpDesignation());
-		}
+//		listEmployee.forEach(emp -> {
+//
+//			System.out.println(emp.getEmpId() + " : " + emp.getEmpName() + " : " + emp.getEmpDesignation());
+//		});
+//		// USING FOR EACH
+//
+//		listEmployee.forEach(emp -> {
+//
+//			System.out.println(emp.getEmpId() + " : " + emp.getEmpName() + " : " + emp.getEmpDesignation());
+//		});
+//
+//		System.out.println("***********************************************************************");
+//		// USING LIST ITERATOR
+//		ListIterator<Employee> listIteratorEmployee = listEmployee.listIterator(listEmployee.size());
+//		while (listIteratorEmployee.hasPrevious()) {
+//
+//			Employee e = listIteratorEmployee.previous();
+//			System.out.println(e.getEmpId() + " : " + e.getEmpName() + " : " + e.getEmpDesignation());
+//		}
+//
+//		System.out.println("***********************************************************************");
+//		// USING FOR EACH REMAINING
+//		Iterator<Employee> itrEmp = listEmployee.iterator();
+//		itrEmp.forEachRemaining(emp -> {
+//
+//			System.out.println(emp.getEmpId() + " : " + emp.getEmpName() + " : " + emp.getEmpDesignation());
+//		});
 
-		System.out.println("***********************************************************************");
-		// USING FOR EACH REMAINING
-		Iterator<Employee> itrEmp = listEmployee.iterator();
-		itrEmp.forEachRemaining(emp -> {
-
-			System.out.println(emp.getEmpId() + " : " + emp.getEmpName() + " : " + emp.getEmpDesignation());
-		});
+//		System.out.println(emp5.equals(emp6));
+//		System.out.println("emp5 hashcode : " + emp5.hashCode());
+//		System.out.println("emp6 hashcode : " + emp6.hashCode());
 
 	}
-
 }
